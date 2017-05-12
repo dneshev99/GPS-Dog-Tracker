e017d21dd290d1684e4c3e2e30b843116fd2c3f6
Dimitur Neshev <d.neshev99@gmail.com>
2017-05-12 20:06:18 +0300

    Android app update
    Added Login working on register

---
+++ app/src/main/java/neshev/dimityr/gps_sms/LoginActivity.java
@@ -0,0 +1,158 @@
+package neshev.dimityr.gps_sms;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+
+
+
+public class LoginActivity extends Activity {
+
+    private TextView errorText;
+    private EditText usernameText,passwordText;
+
+    String [] permissions = {Manifest.permission.RECEIVE_SMS,Manifest.permission.ACCESS_FINE_LOCATION};
+
+    @Override
+    public void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        isGooglePlayServicesAvailable(this);
+
+        if(checkIfAlreadyhavePermission()){
+            Toast.makeText(this,"Already hame permissions",Toast.LENGTH_SHORT).show();
+        }else {
+            requestForSpecificPermission();
+        }
+
+        setTextWidgets();
+        setButtons();
+
+
+    }
+
+
+    public boolean isGooglePlayServicesAvailable(Activity activity) {
+        GoogleApiAvailability googleApiAvailability = GoogleApiAvailability.getInstance();
+        int status = googleApiAvailability.isGooglePlayServicesAvailable(activity);
+        if(status != ConnectionResult.SUCCESS) {
+            if(googleApiAvailability.isUserResolvableError(status)) {
+                googleApiAvailability.getErrorDialog(activity, status, 2404).show();
+            }
+            return false;
+        }
+        return true;
+    }
+
+    private boolean checkIfAlreadyhavePermission() {
+        int locationResult = ContextCompat.checkSelfPermission(this, Manifest.permission_group.LOCATION);
+        int smsResult = ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS);
+        return locationResult == PackageManager.PERMISSION_GRANTED && smsResult == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestForSpecificPermission() {
+        ActivityCompat.requestPermissions(this, permissions, 101);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 101:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this,"Permissions Granted",Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this,"Permissions Not Granted",Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    private void setTextWidgets() {
+        errorText = (TextView) findViewById(R.id.error_text);
+        usernameText = (EditText) findViewById(R.id.username_text);
+        passwordText = (EditText) findViewById(R.id.password_text);
+    }
+
+    private void setButtons() {
+        Button loginButton = (Button) findViewById(R.id.login_button);
+        Button registerButton = (Button) findViewById(R.id.register_button);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = usernameText.getText().toString();
+                String password = passwordText.getText().toString();
+
+                if(username.equals("")){
+                    errorText.setText("Input username.");
+                }else if(password.equals("")){
+                    errorText.setText("Input password.");
+                }else{
+                    getRequest(username,password);
+                }
+            }
+        });
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startRegister = new Intent(getApplicationContext(),RegisterActivity.class);
+                startActivity(startRegister);
+            }
+        });
+    }
+
+    private void getRequest(String username, String password) {
+        String url = "http://tues-gps-app.herokuapp.com/check_login?username=" + username + "&password=" + password;
+
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+
+            StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    handleResponse(response);
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    errorText.setText("Try Again");
+                }
+            });
+            queue.add(request);
+    }
+
+    private void handleResponse(String response) {
+        if (response.equals("incorrect_username")){
+            errorText.setText("Incorrect username.");
+        }else if(response.equals("incorrect_password")){
+            errorText.setText("Incorrect password.");
+        }else if(response.equals("success")){
+            Intent startMenu = new Intent(this,MenuActivity.class);
+            startActivity(startMenu);
+        }
+
+    }
+
+
+}
--- app/src/main/java/neshev/dimityr/gps_sms/MapsActivity.java
+++ app/src/main/java/neshev/dimityr/gps_sms/MapsActivity.java
@@ -1,78 +1,55 @@
 package neshev.dimityr.gps_sms;
 
-import android.Manifest;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.pm.PackageManager;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
-import android.telephony.SmsMessage;
-import android.widget.Toast;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
     private GoogleMap mMap;
     private BroadcastReceiver receiver = null;
+    private Marker dogMarker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        setContentView(R.layout.activity_googlemaps);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-        if(checkIfAlreadyhavePermission()){
-            Toast.makeText(this,"Already hame permissions",Toast.LENGTH_SHORT).show();
-        }else {
-                requestForSpecificPermission();
-        }
-    }
-    private boolean checkIfAlreadyhavePermission() {
-        int result = ContextCompat.checkSelfPermission(this,Manifest.permission.RECEIVE_SMS);
-        return result == PackageManager.PERMISSION_GRANTED;
+
+
     }
 
-    private void requestForSpecificPermission() {
-        ActivityCompat.requestPermissions(this, new String[]{ Manifest.permission.RECEIVE_SMS, Manifest.permission.READ_SMS}, 101);
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        switch (requestCode) {
-            case 101:
-                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Toast.makeText(this,"Permissions Granted",Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(this,"Permissions Not Granted",Toast.LENGTH_SHORT).show();
-                }
-                break;
-            default:
-                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        }
-    }
-
+    @SuppressWarnings("MissingPermission")
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        IntentFilter filter = new IntentFilter("android.provider.Telephony.SMS_RECEIVED");
+        mMap.setMyLocationEnabled(true);
+
+        Intent startSmsActivity = new Intent(getApplicationContext(),SmsService.class);
+        startService(startSmsActivity);
+
+        IntentFilter filter = new IntentFilter("locationUpdates");
 
         receiver = new BroadcastReceiver() {
 
             @Override
             public void onReceive(Context context, Intent intent) {
-                proccessSmS(context, intent);
+                String body = intent.getExtras().get("location").toString();
+                String[] tokens = body.split(" ");
+                LatLng location = new LatLng(Float.parseFloat(tokens[1]),Float.parseFloat(tokens[2]));
+                updateDogMarker(location);
             }
         };
         registerReceiver(receiver, filter);
@@ -81,26 +58,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onDestroy() {
         super.onDestroy();
         unregisterReceiver(receiver);
-    }
-    private void proccessSmS(Context context, Intent intent) {
-        Bundle bundle = intent.getExtras();
-        Object[] smses = (Object[])bundle.get("pdus");
-        for (int i=0;i<smses.length;i++){
-            SmsMessage sms = SmsMessage.createFromPdu((byte[])smses[i]);
-            String body = sms.getMessageBody();
-            if(body.contains("location:")) {
-                AddMarker(body);
-            }
-        }
+        Intent startSmsActivity = new Intent(getApplicationContext(),SmsService.class);
+        stopService(startSmsActivity);
     }
 
-    private void AddMarker(String body) {
-        String[] tokens = body.split(" ");
-        float lat = Float.parseFloat(tokens[1]);
-        float lng = Float.parseFloat(tokens[2]);
-        Toast.makeText(this,"Lat:"+lat+"\n"+"Lng"+lng,Toast.LENGTH_LONG).show();
-        LatLng location = new LatLng(lat,lng);
-        MarkerOptions marker = new MarkerOptions().position(location);
-        mMap.addMarker(marker);
+
+    private void updateDogMarker(LatLng location) {
+        if (dogMarker != null){
+            dogMarker.remove();
+        }
+        MarkerOptions options = new MarkerOptions().position(location);
+        dogMarker = mMap.addMarker(options);
     }
+
 }
